b6f31e98f101da478ec7dd71d25f1660efac1beb
davin <dizqar@gmail.com>
2021-04-05 14:51:11 +0700

    callbacks

---
+++ pg/callback.js
@@ -0,0 +1,40 @@
+const names = ["tony", 'rijal', 'don', 'rendi', 'orton']
+
+const shortNames = names.filter((name) => {
+    return name.length <= 4
+})
+
+const geocode = (address, callback) => {
+    setTimeout(() => {
+        const data = {
+            lat: 0,
+            long: 0
+        }
+        callback(data)
+    }, 2000)
+}
+
+geocode('Philadelphia', (data) => {
+    console.log(data)
+})
+
+
+//
+// Goal: Mess around with the callback pattern
+//
+// 1. Define an add function that accepts the correct arguments
+// 2. Use setTimeout to simulate a 2 second delay
+// 3. After 2 seconds are up, call the callback function with the sum
+// 4. Test your work!
+
+add = (x, y, callback) => {
+    setTimeout(() => {
+        const answer = x + y
+
+        callback(answer)
+    }, 2000)
+}
+
+add(1, 4, (sum) => {
+    console.log(sum) // Should print: 5
+})
--- weather/app.js
+++ weather/app.js
@@ -1,10 +1,23 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=184e621929638ddfe3720394fb6f920e&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=184e621929638ddfe3720394fb6f920e&query=45.000000,%20-63.000000&units=s'
 
 request({
-    url: url
+    url: url,
+    json: true
 }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+    error ?
+        console.log('Toni Hawk is skating now') :
+        console.log(`${response.body.current.weather_descriptions[0]}. Temperature: ${response.body.current.temperature} K, feels like: ${response.body.current.feelslike} K`)
+})
+
+const curl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZGF2aW5yaXpvIiwiYSI6ImNrbXpvaTV2ZjBid28ydHZ2d2FwNzV2eTYifQ.NQHazUeb4Uc7ilTOslF4bg&limit=1'
+
+request({
+    url: curl,
+    json: true
+}, (error, response) => {
+    error ?
+        console.log('Wakanda forever') :
+        console.log(response.body.features[0].center)
 })
