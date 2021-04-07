5b8df35703234426d21e3bc484b1726c7858d7bf
davin <dizqar@gmail.com>
2021-04-07 21:09:40 +0700

    views hbs

--- server/src/app.js
+++ server/src/app.js
@@ -5,7 +5,29 @@ const app = express()
 
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Tont Hawks weather app',
+        name: 'Tont Hawk'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('index', {
+        title: 'About Tont Hawk',
+        name: 'Tont Hawk'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('index', {
+        title: 'Please help Tont Hawk',
+        name: 'Tont Hawk'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send('Tont Hawk is raining cats and dogs')
